6c2d5f27f8c3b908ca975d7f85fc9174bd54212e
chengqhuster <chengqhuster@gmail.com>
2018-10-24 14:06:53 +0800

    chapter3-1-2 Thymeleaf

---
+++ chapter3-1-2/src/main/java/com/chengqhuster/web/HelloController.java
@@ -0,0 +1,22 @@
+package com.chengqhuster.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+    @RequestMapping("/")
+    public String index(ModelMap map) {
+        map.addAttribute("host", "https://github.com/chengqhuster/SpringBoot-Learning");
+        return "index";
+    }
+}
