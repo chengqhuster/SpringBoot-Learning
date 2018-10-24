12bc12a8b795650acbaafcc61faa811b98f11d7d
chengqhuster <chengqhuster@gmail.com>
2018-10-24 13:30:48 +0800

    chapter3-1-1 restful api

--- chapter3-1-1/src/test/java/com/chengqhuster/Chapter311ApplicationTests.java
+++ chapter3-1-1/src/test/java/com/chengqhuster/Chapter311ApplicationTests.java
@@ -21,46 +21,46 @@ import static org.hamcrest.Matchers.equalTo;
 @SpringBootTest(classes = {MockServletContext.class})
 public class Chapter311ApplicationTests {
 
-	private MockMvc mvc;
+    private MockMvc mvc;
 
-	@Before
-	public void setUp() {
-		mvc = MockMvcBuilders.standaloneSetup(
-				new HelloController(),
-				new UserController()).build();
-	}
+    @Before
+    public void setUp() {
+        mvc = MockMvcBuilders.standaloneSetup(
+                new HelloController(),
+                new UserController()).build();
+    }
 
-	@Test
-	public void getHello() throws Exception{
-		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
-				.andExpect(MockMvcResultMatchers.status().isOk())
-				.andExpect(MockMvcResultMatchers.content().string(equalTo("Hello World")));
-	}
-
-	@Test
-	public void testUserController() throws Exception{
-		RequestBuilder request = null;
-
-		//1、get查一下user列表，应该为空
-		request = MockMvcRequestBuilders.get("/users/");
-		mvc.perform(request)
-				.andExpect(MockMvcResultMatchers.status().isOk())
-				.andExpect(MockMvcResultMatchers.content().string(equalTo("[]")));
-
-		//2、post一个user
-		request = MockMvcRequestBuilders.post("/users/")
-				.param("id", "1")
-				.param("name", "测试人员")
-				.param("age", "20");
-		mvc.perform(request)
-				.andExpect(MockMvcResultMatchers.status().isOk())
-				.andExpect(MockMvcResultMatchers.content().string(equalTo("success")));
-
-		// 3、get获取user列表，应该有刚才插入的数据
-		request = MockMvcRequestBuilders.get("/users/");
-		mvc.perform(request)
+    @Test
+    public void getHello() throws Exception{
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-				.andExpect(MockMvcResultMatchers.content().string(equalTo("[{\"id\":1,\"name\":\"测试人员\",\"age\":20}]")));
+                .andExpect(MockMvcResultMatchers.content().string(equalTo("Hello World")));
+    }
+
+    @Test
+    public void testUserController() throws Exception{
+        RequestBuilder request = null;
+
+        //1、get查一下user列表，应该为空
+        request = MockMvcRequestBuilders.get("/users/");
+        mvc.perform(request)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(equalTo("[]")));
+
+        //2、post一个user
+        request = MockMvcRequestBuilders.post("/users/")
+                .param("id", "1")
+                .param("name", "测试人员")
+                .param("age", "20");
+        mvc.perform(request)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(equalTo("success")));
+
+        // 3、get获取user列表，应该有刚才插入的数据
+        request = MockMvcRequestBuilders.get("/users/");
+        mvc.perform(request)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(equalTo("[{\"id\":1,\"name\":\"测试人员\",\"age\":20}]")));
 
         // 4、put修改id为1的user
         request = MockMvcRequestBuilders.put("/users/1")
@@ -70,21 +70,20 @@ public class Chapter311ApplicationTests {
                 .andExpect(MockMvcResultMatchers.content().string(equalTo("success")));
 
 
-		// 5、get一个id为1的user
-		request = MockMvcRequestBuilders.get("/users/1");
-		mvc.perform(request)
-				.andExpect(MockMvcResultMatchers.content().string(equalTo("{\"id\":1,\"name\":\"高级测试人员\",\"age\":30}")));
+        // 5、get一个id为1的user
+        request = MockMvcRequestBuilders.get("/users/1");
+        mvc.perform(request)
+                .andExpect(MockMvcResultMatchers.content().string(equalTo("{\"id\":1,\"name\":\"高级测试人员\",\"age\":30}")));
 
-		// 6、del删除id为1的user
-		request = MockMvcRequestBuilders.delete("/users/1");
-		mvc.perform(request)
+        // 6、del删除id为1的user
+        request = MockMvcRequestBuilders.delete("/users/1");
+        mvc.perform(request)
                 .andExpect(MockMvcResultMatchers.content().string(equalTo("success")));
 
-        //1、get查一下user列表，应该为空
+        //7、get查一下user列表，应该为空
         request = MockMvcRequestBuilders.get("/users/");
         mvc.perform(request)
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string(equalTo("[]")));
-	}
-
+    }
 }
